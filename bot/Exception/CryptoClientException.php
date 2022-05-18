46e8d198cbbeb1c97ae0e82f6ccaf39c3397dd56
Kawamara Mugire <52707074+kawamara1@users.noreply.github.com>
2022-05-18 07:56:21 -0400

    Create CryptoClientException.php

---
+++ bot/Exception/CryptoClientException.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * A simple Twitter bot application which posts hourly status updates for the top 10 cryptocurrencies.
+ *
+ * PHP version >= 7.4
+ *
+ * LICENSE: MIT, see LICENSE file for more information
+ *
+ * @author JR Cologne <kontakt@jr-cologne.de>
+ * @copyright 2021 JR Cologne
+ * @license https://github.com/jr-cologne/CryptoStatus/blob/master/LICENSE MIT
+ * @version v0.9.2-beta
+ * @link https://github.com/jr-cologne/CryptoStatus GitHub Repository
+ *
+ * ________________________________________________________________________________
+ *
+ * CryptoClientException.php
+ *
+ * The Exception of the Crypto client
+ *
+ */
+
+namespace CryptoStatus\Exceptions;
+
+class CryptoClientException extends Exception
+{
+
+}
